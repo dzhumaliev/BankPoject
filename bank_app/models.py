86463f7ff1e308ff80d888e2f613792447084395
isakd <isak.djumaliev65@gmail.com>
2025-04-23 00:19:19 -0700

    Created in model cash_balance and online_balance
    that shows a sum of both.
    
    connected admin panel

--- bank_app/models.py
+++ bank_app/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
 from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    cash_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    online_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    @property
+    def balance(self):
+        return self.cash_balance + self.online_balance
 
     def __str__(self):
-        return f"{self.name} — {self.balance}₽"
+        return f"{self.name} — {self.balance}$ (Cash: {self.cash_balance}$, Online: {self.online_balance}$)"
